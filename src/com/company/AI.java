c30335779eeda1c0c330785060bd74524dfe48b4
Argen Detoito <adetoito2@gmail.com>
2016-12-19 18:58:24 -0500

    Third commit.
    Need to work on symbol placements for AI.

--- src/com/company/AI.java
+++ src/com/company/AI.java
@@ -1,27 +1,15 @@
 package com.company;
 
-import java.util.*;
-
 public class AI {
+    public static int getRow (String [] row1, String [] row2, String [] row3) {
+        int row = 1;
+        // Determines what row to place symbol.
+        return row;
+    }
 
-    public static int makeDecision (String [] a) {
-        Random rand = new Random();
-        int [] b = {-1, -1, -1, -1, -1, -1, -1, -1, -1};
-
-        for (int i = 0; i < a.length; i++) {
-            if (a[i].equals("X")) {
-                b[i] = i + 1;
-            }
-        }
-
-        int location = 1;
-        boolean action = true;
-        while (action) {
-            location = b[rand.nextInt(a.length)];
-            if (location != -1) {
-                action = false;
-            }
-        }
-        return location;
+    public static int getColumn (int row, String [] row1, String [] row2, String [] row3) {
+        int column = 1;
+        // Determines what column to place symbol.
+        return column;
     }
 }
